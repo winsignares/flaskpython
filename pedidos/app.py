6c07221fb6bba96808b09df10f8308972ce2b6a2
= <=>
2021-11-14 09:34:31 -0500

    mi segundo commit

--- pedidos/app.py
+++ pedidos/app.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/prueba'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
